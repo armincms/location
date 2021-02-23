60d82a266f7bba9fe75f56556b4a6ab6c997d410
Ismail Zare <zarehesmaiel@gmail.com>
2021-02-23 11:51:45 +0330

    Renames migrations names

--- database/migrations/2018_10_24_000000_create_location_countries_table.php
+++ database/migrations/2018_10_24_000000_create_location_countries_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration; 
 
-class CreateCountriesTable extends Migration
+class CreateLocationCountriesTable extends Migration
 {
     /**
      * Run the migrations.
